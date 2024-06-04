a2eb48688a12ba9df2816fbad8864f30283489ed
mar <137989121+mar5608@users.noreply.github.com>
2024-06-04 13:43:59 +0900

    中級編DAY22スマホ時のドロワーメニューを作る 04. ドロワーのJavaScriptをコーディング jQueryの場合　途中

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
 // alert("おはよう");
+
+jQuery("#js-drawer-icon").on("click", function (e) {
+  e.preventDefault();
+  jQuery("js-drawer-icon").toggleClass("is-checked");
+  jQuery("js-drawer-content").toggleClass("is-checked");
+});
+
+// jQuery("#js-drawer-icon").on("click", function (e) {
+//     e.preventDefault();
+//     jQuery("#js-drawer-icon").toggleClass("is-checked");
+//     jQuery("#js-drawer-content").toggleClass("is-checked");
+//   });
+
+// JavaScript
+// document
+//   .querySelector("#js-drawer-icon")
+//   .addEventListener("click", function (e) {
+//     e.preventDefault(); //標準の動きを無効化しておく
+//     //iconに対してつけ外しする
+//     document.querySelector("#js-drawer-icon").classList.toggle("is-checked");
+//     document.querySelector("#js-drawer-content").classList.toggle("is-checked");
+//   });
