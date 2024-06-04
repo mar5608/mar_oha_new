d33fd38db97e5956dc3becc86da92616eaa686e1
mar <137989121+mar5608@users.noreply.github.com>
2024-06-04 13:52:36 +0900

    中級編DAY22スマホ時のドロワーメニューを作る　完了

--- js/script.js
+++ js/script.js
@@ -1,23 +1,22 @@
 // alert("おはよう");
 
-jQuery("#js-drawer-icon").on("click", function (e) {
-  e.preventDefault();
-  jQuery("js-drawer-icon").toggleClass("is-checked");
-  jQuery("js-drawer-content").toggleClass("is-checked");
-});
-
+/* jQuery */
 // jQuery("#js-drawer-icon").on("click", function (e) {
-//     e.preventDefault();
-//     jQuery("#js-drawer-icon").toggleClass("is-checked");
-//     jQuery("#js-drawer-content").toggleClass("is-checked");
-//   });
+//   e.preventDefault();
+//   jQuery("#js-drawer-icon").toggleClass("is-checked");
+//   jQuery("#js-drawer-content").toggleClass("is-checked");
+// });
 
-// JavaScript
-// document
-//   .querySelector("#js-drawer-icon")
-//   .addEventListener("click", function (e) {
-//     e.preventDefault(); //標準の動きを無効化しておく
-//     //iconに対してつけ外しする
-//     document.querySelector("#js-drawer-icon").classList.toggle("is-checked");
-//     document.querySelector("#js-drawer-content").classList.toggle("is-checked");
-//   });
+const drawerIcon = document.querySelector("#js-drawer-icon");
+const drawerContent = document.querySelector("#js-drawer-content");
+
+/* JavaScript */
+if (drawerIcon) {
+  //drawerIconがあった時のみ実施
+  drawerIcon.addEventListener("click", function (e) {
+    e.preventDefault(); //標準の動きを無効化しておく
+    //iconに対してつけ外しする
+    drawerIcon.classList.toggle("is-checked");
+    drawerContent.classList.toggle("is-checked");
+  });
+}
