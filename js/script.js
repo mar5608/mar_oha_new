0c40d00f9453e9d0e653678005227e1c68fb5666
mar <137989121+mar5608@users.noreply.github.com>
2024-06-05 22:51:42 +0900

    中級編DAY22スマホ時のドロワーメニューを作る 07. Q&AセクションのJavaScriptまで完了

--- js/script.js
+++ js/script.js
@@ -1,22 +1,37 @@
 // alert("おはよう");
 
 /* jQuery */
-// jQuery("#js-drawer-icon").on("click", function (e) {
-//   e.preventDefault();
-//   jQuery("#js-drawer-icon").toggleClass("is-checked");
-//   jQuery("#js-drawer-content").toggleClass("is-checked");
-// });
+jQuery("#js-drawer-icon").on("click", function (e) {
+  e.preventDefault();
+  jQuery("#js-drawer-icon").toggleClass("is-checked");
+  jQuery("#js-drawer-content").toggleClass("is-checked");
+});
 
-const drawerIcon = document.querySelector("#js-drawer-icon");
-const drawerContent = document.querySelector("#js-drawer-content");
+jQuery(".js-accordion").on("click", function (e) {
+  e.preventDefault();
+
+  if (jQuery(this).parent().hasClass("is-open")) {
+    // thisで押した要素を取得する
+    jQuery(this).parent().removeClass("is-open");
+    // スライドしながら戻る
+    jQuery(this).next().slideUp();
+  } else {
+    jQuery(this).parent().addClass("is-open");
+    jQuery(this).next().slideDown();
+  }
+});
 
 /* JavaScript */
-if (drawerIcon) {
-  //drawerIconがあった時のみ実施
-  drawerIcon.addEventListener("click", function (e) {
-    e.preventDefault(); //標準の動きを無効化しておく
-    //iconに対してつけ外しする
-    drawerIcon.classList.toggle("is-checked");
-    drawerContent.classList.toggle("is-checked");
-  });
-}
+
+// const drawerIcon = document.querySelector("#js-drawer-icon");
+// const drawerContent = document.querySelector("#js-drawer-content");
+
+// if (drawerIcon) {
+//   //drawerIconがあった時のみ実施
+//   drawerIcon.addEventListener("click", function (e) {
+//     e.preventDefault(); //標準の動きを無効化しておく
+//     //iconに対してつけ外しする
+//     drawerIcon.classList.toggle("is-checked");
+//     drawerContent.classList.toggle("is-checked");
+//   });
+// }
