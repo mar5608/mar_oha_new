11e219a70e459d5274711757b0b12274936a8323
mar <137989121+mar5608@users.noreply.github.com>
2024-06-06 17:22:42 +0900

    中級編 DAY25モーダルを作る 完了

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 // alert("おはよう");
 
 /* jQuery */
+/*js-drawer*/
 jQuery("#js-drawer-icon").on("click", function (e) {
   e.preventDefault();
   jQuery("#js-drawer-icon").toggleClass("is-checked");
@@ -21,21 +22,6 @@ jQuery(".js-accordion").on("click", function (e) {
   }
 });
 
-/** JavaScript */
-
-// const drawerIcon = document.querySelector("#js-drawer-icon");
-// const drawerContent = document.querySelector("#js-drawer-content");
-
-// if (drawerIcon) {
-//   //drawerIconがあった時のみ実施
-//   drawerIcon.addEventListener("click", function (e) {
-//     e.preventDefault(); //標準の動きを無効化しておく
-//     //iconに対してつけ外しする
-//     drawerIcon.classList.toggle("is-checked");
-//     drawerContent.classList.toggle("is-checked");
-//   });
-// }
-
 /**  swiper */
 const swiper = new Swiper("#js-gallery-swiper", {
   spaceBetween: 82, //横のカードとの間
@@ -53,3 +39,55 @@ const swiper = new Swiper("#js-gallery-swiper", {
     prevEl: "#js-gallery-prev",
   },
 });
+
+/** modal */
+jQuery(".js-modal-open").on("click", function (e) {
+  e.preventDefault();
+
+  jQuery("#js-about-modal")[0].showModal();
+});
+
+jQuery(".js-modal-close").on("click", function (e) {
+  e.preventDefault();
+
+  jQuery("#js-about-modal")[0].close();
+});
+
+/** JavaScript */
+/*js-drawer*/
+// const drawerIcon = document.querySelector("#js-drawer-icon");
+// const drawerContent = document.querySelector("#js-drawer-content");
+
+// if (drawerIcon) {
+//   //drawerIconがあった時のみ実施
+//   drawerIcon.addEventListener("click", function (e) {
+//     e.preventDefault(); //標準の動きを無効化しておく
+//     //iconに対してつけ外しする
+//     drawerIcon.classList.toggle("is-checked");
+//     drawerContent.classList.toggle("is-checked");
+//   });
+// }
+
+/** modal */
+// const modalOpenItems = document.querySelectorAll(".js-modal-open");
+// const modalCloseItems = document.querySelectorAll(".js-modal-close");
+// const aboutModal = document.querySelector("#js-about-modal");
+
+// modalOpenItems.forEach(function (modalOpenItem) {
+//   modalOpenItem.addEventListener("click", function (e) {
+//     e.preventDefault();
+
+//     if (aboutModal) {
+//       aboutModal.showModal();
+//     }
+//   });
+// });
+
+// modalCloseItems.forEach(function (modalCloseItem) {
+//   modalCloseItem.addEventListener("click", function (e) {
+//     e.preventDefault();
+//     if (aboutModal) {
+//       aboutModal.close();
+//     }
+//   });
+// });
