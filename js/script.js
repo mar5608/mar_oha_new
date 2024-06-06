60450d761876f3ee66e59f9799b912d06e6b3182
mar <137989121+mar5608@users.noreply.github.com>
2024-06-06 23:03:51 +0900

    中級編DAY26　スムーススクロール　完了

--- js/script.js
+++ js/script.js
@@ -53,7 +53,47 @@ jQuery(".js-modal-close").on("click", function (e) {
   jQuery("#js-about-modal")[0].close();
 });
 
-/** JavaScript */
+/** スムーススクロール*/
+/** drawer 制御 */
+jQuery('#js-drawer-content a[href^="#"]').on("click", function (e) {
+  e.preventDefault();
+  jQuery("#js-drawer-icon").removeClass("is-checked");
+  jQuery("#js-drawer-content").removeClass("is-checked");
+});
+
+jQuery('a[href^="#"]').on("click", function (e) {
+  const speed = 300;
+  const id = jQuery(this).attr("href");
+  const target = jQuery("#" == id ? "html" : id);
+  const position = jQuery(target).offset().top;
+  jQuery("html,body").animate(
+    {
+      scrollTop: position,
+    },
+    speed,
+    "swing" // swing or liner
+  );
+  // jQuery("html,body").animate(
+  //   {
+  //     scrollTop: 0,
+  //   },
+  //   1000,
+  //   "swing" // swing or liner
+  // );
+});
+
+/** topに戻る */
+jQuery(window).on("scroll", function () {
+  if (100 < jQuery(window).scrollTop()) {
+    jQuery("#js-pagetop").addClass("is-show");
+  } else {
+    jQuery("#js-pagetop").removeClass("is-show");
+  }
+});
+
+/**
+ * JavaScript
+ * */
 /*js-drawer*/
 // const drawerIcon = document.querySelector("#js-drawer-icon");
 // const drawerContent = document.querySelector("#js-drawer-content");
@@ -92,24 +132,12 @@ jQuery(".js-modal-close").on("click", function (e) {
 //   });
 // });
 
-/** スムーススクロール*/
-jQuery('a[href^="#"]').on("click", function (e) {
-  const speed = 300;
-  const id = jQuery(this).attr("href");
-  const target = jQuery("#" == id ? "html" : id);
-  const position = jQuery(target).offset().top;
-  jQuery("html,body").animate(
-    {
-      scrollTop: position,
-    },
-    speed,
-    "swing" // swing or liner
-  );
-  // jQuery("html,body").animate(
-  //   {
-  //     scrollTop: 0,
-  //   },
-  //   1000,
-  //   "swing" // swing or liner
-  // );
-});
+/** topに戻る */
+// const pageTop = document.querySelector("#js-pagetop");
+// window.addEventListener("scroll", function () {
+//   if (100 < this.window.scrollY) {
+//     pageTop.classList.add("is-show");
+//   } else {
+//     pageTop.classList.remove("is-show");
+//   }
+// });
